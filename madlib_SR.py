f1bb2d85a4d7a37eff5873dc628c47e199432af5
srosenfeld <srosenfeld@users.noreply.github.com>
2017-11-07 09:43:19 -0500

    First upload
    Here we go!

---
+++ madlib_SR.py
@@ -0,0 +1,17 @@
+### MAD LIBS ###
+### Source: https://i.pinimg.com/736x/41/a6/c0/41a6c07c0984682b69a98749edb65c3b--funny-mad-libs-pizza-pizza.jpg
+
+print("Give me a noun")
+noun1 = input()
+
+print("Write an adjective.")
+adj1 = input()
+
+print("Name a movie")
+movie = input()
+
+
+print("The " + noun1 + " went to the "+ adj1 + " store.")
+print("We all wanted to see " + movie + " but the tickets sold out.")
+
+
---
+++ personality_survey_SR.py
@@ -0,0 +1,37 @@
+print("What's your favorite sport?")
+sport = input()
+
+if sport == "Hockey":
+    print("That's my favorite sport too!")
+elif sport == "Tennis":
+    print("Tennis is my other favorite sport to play!")
+elif sport == "Crew":
+    print("Woah I love crew! You do too? Awesome!")
+else:
+    print(sport + " sounds fun to play.")
+
+
+
+print("What's your favorite subject?")
+subject = input()
+
+if subject == "Art" or subject == "Math":
+    print("I love art and math!")
+else:
+    print(subject + " is a great class too.")
+
+
+print("What's your fav food?")
+food = input()
+
+if food == "candy":
+    print("Oh boy")
+elif food == "crepe":
+    print("What's your favorite kind of crepe?")
+    favfood = input()
+    if favfood == "nutella and banana":
+        print("Yum!")
+    else:
+        print("That's nice.")
+elif food == "orange":
+    print("Orange ya glad I didn't say banana?")
